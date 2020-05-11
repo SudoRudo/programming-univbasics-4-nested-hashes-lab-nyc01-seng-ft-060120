56178fd318ed3a66d29ee7c32ea686e1c33e1e0e
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-11 06:36:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,12 +63,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -86,7 +80,7 @@ def adding_matz
       }
     }
 
-  programmer_hash.unshift(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]},
   
   programmer_hash
 end
