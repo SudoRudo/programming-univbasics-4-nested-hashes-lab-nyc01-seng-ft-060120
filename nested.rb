47648957d068c20aa63eb647f817ce68934bdaaa
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-11 06:41:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
       }
     }
 
-  programmer_hash[0] = :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]},
+  programmer_hash[0] = ':yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}',
   
   programmer_hash
 end
